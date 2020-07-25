a8f918b63904a3a88bd8bef040cf1ea1d3b5ede0
Debonairesnake6 <canadianboyz@hotmail.ca>
2020-07-25 15:33:35 -0400

    Completed initial item creation with modifiers.

--- src/items.py
+++ src/items.py
@@ -1,55 +1,59 @@
 """
-This will contaion all of the information about all of the items for the game
+This will contain all of the information about all of the items for the game.
 """
-
-
-def create_item(name: str, modifiers: dict = False):
-    """
-    This will create an item based on the given name
-
-    :param name: Name of the item
-    :param modifiers: Any modifiers to add to the base stats of an item
-    """
-    base_items = {
-        'sword': {'attack': 5, 'defense': 1, 'attack_speed': 2, 'attack_range': 3},
-        'shield': {'attack': 1, 'defense': 3, 'attack_speed': 1, 'attack_range': 1},
-        'bow': {'attack': 4, 'attack_speed': 2, 'attack_range': 10}
-    }
-    name = name
-    modifiers = modifiers
-    item_stats = base_items[name]
-
-    # Add modifiers
-    for stat, value in modifiers.items():
-        item_stats[stat] += value
-
-    # Create the Item
-
-    item = BaseItem()
 
 
 class BaseItem:
     """
-    This will include any basic stats about an item
+    This will include any basic stats about an item. The current base items are:
+    Sword, Shield, Bow
     """
 
-    def __init__(self, name: str, attack: int = 0, defense: int = 0, attack_speed: int = 0, attack_range: int = 0,
-                 extra_targets: int = 0, heal: int = 0):
+    def __init__(self, name: str, modifiers: dict = False):
         """
-        This will include any basic stats about an item
+        This will include any basic stats about an item. The current base items are:
+        Sword, Shield, Bow
 
         :param name: Name of the item
-        :param attack: Attack damage
-        :param defense: Dense stat
-        :param attack_speed: How often the item can attack
-        :param attack_range: Range of the item
-        :param extra_targets: How many additional targets the item will damage
-        :param heal: How much the item will heal
+        :param modifiers: Any modifiers to add to the base stats of an item
         """
         self.name = name
-        self.attack = attack
-        self.defense = defense
-        self.attack_speed = attack_speed
-        self.attack_range = attack_range
-        self.extra_targets = extra_targets
-        self.heal = heal
+        self.stats = {
+            'attack': 0,
+            'defense': 0,
+            'attack_speed': 0,
+            'attack_range': 0,
+            'extra_targets': 0,
+            'heal': 0
+        }
+        self.modifiers = modifiers
+
+        self.create_basic_item()
+
+        if modifiers is not False:
+            self.add_modifiers()
+
+    def create_basic_item(self):
+        """
+        Create an item with it's basic stats
+        """
+        base_items = {
+            'sword': {'attack': 5, 'defense': 1, 'attack_speed': 2, 'attack_range': 3},
+            'shield': {'attack': 1, 'defense': 3, 'attack_speed': 1, 'attack_range': 1},
+            'bow': {'attack': 4, 'attack_speed': 2, 'attack_range': 10}
+        }
+        for stat, value in base_items[self.name].items():
+            self.stats[stat] = value
+
+    def add_modifiers(self):
+        """
+        Add any additional modifiers to the base stats of the weapon
+        """
+        for stat, value in self.modifiers.items():
+            self.stats[stat] += value
+
+
+if __name__ == '__main__':
+    sword = BaseItem('sword')
+    bow = BaseItem('bow', {'attack': 1})
+    print()
